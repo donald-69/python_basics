013eec3e0b095644632c01a06e40f02d6ad8c292
Donald <dkirui594@gmail.com>
2025-03-18 00:23:27 -0700

    Python basics

---
+++ number_functions.py
@@ -0,0 +1,12 @@
+import math
+
+x = 69
+
+print(math.sqrt(x))
+print(math.pi)
+print(math.floor(78.88))
+print(math.ceil(78.28))
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,12 @@
+message = "hello,how are you doing?"
+print(message)
+#functions
+print (message.capitalize())
+print(message.upper())
+print(message.lower())
+print(message.islower())
+print(message.isalpha())
+print(message.count("a"))
+print(message.title())
+print(message.replace("doing","going"))
+print(message)
